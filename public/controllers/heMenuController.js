3806e16cd9e307c3bab9b0620b135d811e6ca28b
Mushrath Mohammad <Mushrath.Mohammad@td.com>
2015-12-22 19:17:51 -0500

    Added Form Validation

--- public/controllers/heMenuController.js
+++ public/controllers/heMenuController.js
@@ -182,7 +182,120 @@
                 $scope.itemsPerPage = num;
                 $scope.currentPage = 1; //reset to first paghe
             }
-        }]);
+        }])
+
+        .controller('TodoCtrl',['$scope',function($scope){
+
+            $scope.isCollapsed5 = false;
+
+            $scope.todos = [
+                {text:'learn angular', done:true},
+                {text:'build an angular app', done:false}];
+
+            $scope.addTodo = function() {
+                $scope.todos.push({text:$scope.todoText, done:false});
+                $scope.todoText = '';
+            };
+
+            $scope.remaining = function() {
+                var count = 0;
+                angular.forEach($scope.todos, function(todo) {
+                    count += todo.done ? 0 : 1;
+                });
+                return count;
+            };
+
+            $scope.archive = function() {
+                var oldTodos = $scope.todos;
+                $scope.todos = [];
+                angular.forEach(oldTodos, function(todo) {
+                    if (!todo.done) $scope.todos.push(todo);
+                });
+            };
+
+        }])
+
+
+        //Contact Service && Controller
+
+        .service('ContactService', function () {
+            //to create unique contact id
+            var uid = 1;
+
+            //contacts array to hold list of all contacts
+            var contacts = [{
+                id: 0,
+                'name': 'Megha',
+                'email': 'hello@gmail.com',
+                'phone': '123-2343-44'
+            }];
+
+            //save method create a new contact if not already exists
+            //else update the existing object
+            this.save = function (contact) {
+                if (contact.id == null) {
+                    //if this is new contact, add it in contacts array
+                    contact.id = uid++;
+                    contacts.push(contact);
+                } else {
+                    //for existing contact, find this contact using id
+                    //and update it.
+                    for (var i in contacts) {
+                        if (contacts[i].id == contact.id) {
+                            contacts[i] = contact;
+                        }
+                    }
+                }
+
+            };
+
+            //simply search contacts list for given id
+            //and returns the contact object if found
+            this.get = function (id) {
+                for (var i in contacts) {
+                    if (contacts[i].id == id) {
+                        return contacts[i];
+                    }
+                }
+
+            };
+
+            //iterate through contacts list and delete
+            //contact if found
+            this.delete = function (id) {
+                for (var i in contacts) {
+                    if (contacts[i].id == id) {
+                        contacts.splice(i, 1);
+                    }
+                }
+            };
+
+            //simply returns the contacts list
+            this.list = function () {
+                return contacts;
+            };
+        })
+
+        .controller('ContactController',  function ($scope, ContactService) {
+
+            $scope.contacts = ContactService.list();
+
+            $scope.saveContact = function () {
+                ContactService.save($scope.newcontact);
+                $scope.newcontact = {};
+            };
+
+
+            $scope.delete = function (id) {
+                ContactService.delete(id);
+                if ($scope.newcontact.id == id) $scope.newcontact = {};
+            };
+
+
+            $scope.edit = function (id) {
+                $scope.newcontact = angular.copy(ContactService.get(id));
+            };
+        });
 
 
 })();
