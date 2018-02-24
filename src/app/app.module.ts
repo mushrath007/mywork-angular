3d2b477cc71bd0e7c144272925dae071da003dba
Mushrath Mohammad <Mushrath.Mohammad@td.com>
2018-02-24 06:37:27 -0500

    build fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,8 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
-
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroService }          from './hero.service';
 import { MessageService }       from './message.service';
 import { MessagesComponent }    from './messages/messages.component';
@@ -39,8 +35,6 @@ import { ServicesComponent } from './services/services.component';
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
     MessagesComponent,
     ExperimentComponent,
     AngularTwoFormsComponent,
