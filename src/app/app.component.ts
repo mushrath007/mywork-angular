a522374b9d5f0e4f734a9d3e639b322b9dd9a7b9
Mushrath Mohammad <Mushrath.Mohammad@td.com>
2018-03-30 16:10:51 -0400

    google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -9,7 +9,7 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class AppComponent {
 
-  constructor(private toastr: ToastrService){
+  constructor(private toastr: ToastrService, public http: Http){
     //this.loadComponent = false;
     //this.showForm = true;
   }
@@ -19,6 +19,53 @@ export class AppComponent {
       timeOut: 5000,
     });
   }
+
+  gaGetCountry() {
+
+    this.http.post('https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A172611116&start-date=30daysAgo&end-date=2018-03-30&metrics=ga%3Ausers&access_token=ya29.GluOBdb6FgBlw7Q3zxXWjhVFL4tbtiq89CIRGjdyt7kSpQRVpE0X2vJulvmUMEwUzTQr36fZvgSL499fNMIMJBg2H4khWBwHuSp3UehV9mWOAu7HCEd9TeSAVUh2','','') // google script code follow https://github.com/dwyl/learn-to-send-email-via-google-script-html-no-server/blob/master/index.html for more details
+      .subscribe(
+        res => {
+          //drawRegionsMap(res);
+          console.log(res.json());
+        }
+      );
+  }
+
+//  drawRegionsMap(data) {
+//
+//    var head = data[0];
+//    head[0] = 'Country';
+//    head[1] = 'Users';
+//    for (var i = 1; i < data.length; i++) {
+//      var d = data[i];
+//      d[1] = Number(d[1]);
+//    }
+//
+//    var chartData = google.visualization.arrayToDataTable(data);
+//    var options = {
+//      title: 'My Website is viewed from,',
+//      domain: '{{Country Code eg: IN for India}}',
+//      tooltip: {
+//        textStyle: {
+//          color: 'navy'
+//        },
+//        showColorCode: true
+//      },
+//      legend: {
+//        textStyle: {
+//          color: 'navy',
+//          fontSize: 12
+//        }
+//      },
+//      colorAxis: {
+//        colors: ['#00FFFF', '#0000FF']
+//      }
+//    };
+//
+//    var chart = new google.visualization.GeoChart(document.getElementById('countryChart'));
+//
+//    chart.draw(chartData, options);
+//}
 
   showInfo() {
     this.toastr.info('It means you can see this application in offline mode :', 'This application supports Service Workers)', {
